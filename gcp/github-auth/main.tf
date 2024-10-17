2b8e3e400b1ea5e55e07d4ae06f8f7669cfa2a0b
GergelyKalmar <gergely.kalmar@logikal.io>
2024-10-17 16:55:43 +0200

    Add condition

--- gcp/github-auth/main.tf
+++ gcp/github-auth/main.tf
@@ -24,11 +24,12 @@ resource "google_iam_workload_identity_pool_provider" "github" {
   workload_identity_pool_id = google_iam_workload_identity_pool.ci_cd.workload_identity_pool_id
   workload_identity_pool_provider_id = "github-actions"
   display_name = "GitHub Actions"
+  attribute_condition = "assertion.repository_owner == '${var.github_organization}'"
   attribute_mapping = {
     "google.subject" = "assertion.sub"
     "attribute.actor" = "assertion.actor"
-    "attribute.aud" = "assertion.aud"
     "attribute.repository" = "assertion.repository"
+    "attribute.repository_owner" = "assertion.repository_owner"
   }
   oidc {
     issuer_uri = "https://token.actions.githubusercontent.com"
