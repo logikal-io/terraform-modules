cda16303dd2cd682a6510cde9f3544382511c44d
CristinaNeacsu <cristina.neacsu@logikal.io>
2025-11-28 12:54:36 +0100

    Add metrics and update branch

--- gcp/cloud-composer/monitoring.tf
+++ gcp/cloud-composer/monitoring.tf
@@ -11,13 +11,10 @@ resource "google_monitoring_dashboard" "this" {
       "environment_name": google_composer_environment.this.name,
       "alert_policy_name": {
         "scheduler_heartbeats": google_monitoring_alert_policy.scheduler_heartbeats.name,
-        "database_cpu_usage_time": google_monitoring_alert_policy.database_cpu_usage_time.name,
         "parse_error_count": google_monitoring_alert_policy.parse_error_count.name,
-        "finished_task_instance_count": google_monitoring_alert_policy.finished_task_instance_count.name,
-        "executor_running_tasks": google_monitoring_alert_policy.executor_running_tasks.name,
         "failed_sla_callback_notifications": google_monitoring_alert_policy.failed_sla_callback_notifications.name,
-        "unfinished_task_instances": google_monitoring_alert_policy.unfinished_task_instances.name,
         "orphaned_task_count": google_monitoring_alert_policy.orphaned_task_count.name,
+        "dag_run_schedule_delay": google_monitoring_alert_policy.dag_run_schedule_delay.name,
       },
     },
   )
@@ -58,38 +55,6 @@ resource "google_monitoring_alert_policy" "scheduler_heartbeats" {
   depends_on = [google_project_service.monitoring]
 }
 
-resource "google_monitoring_alert_policy" "database_cpu_usage_time" {
-  display_name = "${google_composer_environment.this.name}-database-cpu-usage-time"
-  combiner = "OR"
-  conditions {
-    display_name = "Database CPU usage time"
-    condition_threshold {
-      threshold_value = 0.8
-      duration = "300s"
-      comparison = "COMPARISON_GT"
-      trigger {
-        count = 1
-      }
-      aggregations {
-        alignment_period = "60s"
-        cross_series_reducer = "REDUCE_PERCENTILE_99"
-        group_by_fields = ["resource.label.environment_name"]
-        per_series_aligner = "ALIGN_RATE"
-      }
-      filter = join(" ", [
-        "resource.type = \"cloud_composer_environment\"",
-        "AND resource.labels.environment_name = \"${google_composer_environment.this.name}\"",
-        "AND metric.type = \"composer.googleapis.com/environment/database/cpu/usage_time\"",
-      ])
-      evaluation_missing_data = "EVALUATION_MISSING_DATA_INACTIVE"
-    }
-  }
-  severity = var.alert_severity
-  notification_channels = var.alert_notification_channel_ids
-
-  depends_on = [google_project_service.monitoring]
-}
-
 resource "google_monitoring_alert_policy" "parse_error_count" {
   display_name = "${google_composer_environment.this.name}-parse-error-count"
   combiner = "OR"
@@ -112,70 +77,6 @@ resource "google_monitoring_alert_policy" "parse_error_count" {
         "resource.type = \"cloud_composer_environment\"",
         "AND resource.labels.environment_name = \"${google_composer_environment.this.name}\"",
         "AND metric.type = \"composer.googleapis.com/environment/dag_processing/parse_error_count\"",
-      ])
-      evaluation_missing_data = "EVALUATION_MISSING_DATA_INACTIVE"
-    }
-  }
-  severity = var.alert_severity
-  notification_channels = var.alert_notification_channel_ids
-
-  depends_on = [google_project_service.monitoring]
-}
-
-resource "google_monitoring_alert_policy" "finished_task_instance_count" {
-  display_name = "${google_composer_environment.this.name}-finished-task-instance-count"
-  combiner = "OR"
-  conditions {
-    display_name = "Finished task instance count"
-    condition_threshold {
-      threshold_value = 0
-      duration = "600s"
-      comparison = "COMPARISON_LT"
-      trigger {
-        count = 1
-      }
-      aggregations {
-        alignment_period = "300s"
-        cross_series_reducer = "REDUCE_SUM"
-        group_by_fields = ["resource.label.environment_name"]
-        per_series_aligner = "ALIGN_DELTA"
-      }
-      filter = join(" ", [
-        "resource.type = \"cloud_composer_environment\"",
-        "AND resource.labels.environment_name = \"${google_composer_environment.this.name}\"",
-        "AND metric.type = \"composer.googleapis.com/environment/finished_task_instance_count\"",
-      ])
-      evaluation_missing_data = "EVALUATION_MISSING_DATA_INACTIVE"
-    }
-  }
-  severity = var.alert_severity
-  notification_channels = var.alert_notification_channel_ids
-
-  depends_on = [google_project_service.monitoring]
-}
-
-resource "google_monitoring_alert_policy" "executor_running_tasks" {
-  display_name = "${google_composer_environment.this.name}-executor-running-tasks"
-  combiner = "OR"
-  conditions {
-    display_name = "Executor running tasks"
-    condition_threshold {
-      threshold_value = 10 # not sure
-      duration = "600s"
-      comparison = "COMPARISON_GT"
-      trigger {
-        count = 1
-      }
-      aggregations {
-        alignment_period = "60s"
-        cross_series_reducer = "REDUCE_SUM"
-        group_by_fields = ["resource.label.environment_name"]
-        per_series_aligner = "ALIGN_SUM"
-      }
-      filter = join(" ", [
-        "resource.type = \"cloud_composer_environment\"",
-        "AND resource.labels.environment_name = \"${google_composer_environment.this.name}\"",
-        "AND metric.type = \"composer.googleapis.com/environment/executor/running_tasks\"",
       ])
       evaluation_missing_data = "EVALUATION_MISSING_DATA_INACTIVE"
     }
@@ -218,38 +119,6 @@ resource "google_monitoring_alert_policy" "failed_sla_callback_notifications" {
   depends_on = [google_project_service.monitoring]
 }
 
-resource "google_monitoring_alert_policy" "unfinished_task_instances" {
-  display_name = "${google_composer_environment.this.name}-unfinished-task-instances"
-  combiner = "OR"
-  conditions {
-    display_name = "Unfinished task instances"
-    condition_threshold {
-      threshold_value = 5
-      duration = "600s"
-      comparison = "COMPARISON_GT"
-      trigger {
-        count = 1
-      }
-      aggregations {
-        alignment_period = "300s"
-        cross_series_reducer = "REDUCE_SUM"
-        group_by_fields = ["resource.label.environment_name"]
-        per_series_aligner = "ALIGN_SUM"
-      }
-      filter = join(" ", [
-        "resource.type = \"cloud_composer_environment\"",
-        "AND resource.labels.environment_name = \"${google_composer_environment.this.name}\"",
-        "AND metric.type = \"composer.googleapis.com/environment/unfinished_task_instances\"",
-      ])
-      evaluation_missing_data = "EVALUATION_MISSING_DATA_INACTIVE"
-    }
-  }
-  severity = var.alert_severity
-  notification_channels = var.alert_notification_channel_ids
-
-  depends_on = [google_project_service.monitoring]
-}
-
 resource "google_monitoring_alert_policy" "orphaned_task_count" {
   display_name = "${google_composer_environment.this.name}-orphaned-task-count"
   combiner = "OR"
@@ -272,6 +141,70 @@ resource "google_monitoring_alert_policy" "orphaned_task_count" {
         "resource.type = \"cloud_composer_environment\"",
         "AND resource.labels.environment_name = \"${google_composer_environment.this.name}\"",
         "AND metric.type = \"composer.googleapis.com/environment/scheduler/task/orphan_count\"",
+      ])
+      evaluation_missing_data = "EVALUATION_MISSING_DATA_INACTIVE"
+    }
+  }
+  severity = var.alert_severity
+  notification_channels = var.alert_notification_channel_ids
+
+  depends_on = [google_project_service.monitoring]
+}
+
+resource "google_monitoring_alert_policy" "dag_run_schedule_delay" {
+  display_name = "${google_composer_environment.this.name}-dag-run-schedule-delay"
+  combiner = "OR"
+  conditions {
+    display_name = "DAG run schedule delay"
+    condition_threshold {
+      threshold_value = 60
+      duration = "60s"
+      comparison = "COMPARISON_GT"
+      trigger {
+        count = 1
+      }
+      aggregations {
+        alignment_period = "300s"
+        cross_series_reducer = "REDUCE_SUM"
+        group_by_fields = ["resource.label.environment_name"]
+        per_series_aligner = "ALIGN_DELTA"
+      }
+      filter = join(" ", [
+        "resource.type = \"cloud_composer_environment\"",
+        "AND resource.labels.environment_name = \"${google_composer_environment.this.name}\"",
+        "AND metric.type = \"composer.googleapis.com/workflow/schedule_delay\"",
+      ])
+      evaluation_missing_data = "EVALUATION_MISSING_DATA_INACTIVE"
+    }
+  }
+  severity = var.alert_severity
+  notification_channels = var.alert_notification_channel_ids
+
+  depends_on = [google_project_service.monitoring]
+}
+
+resource "google_monitoring_alert_policy" "dag_file_load_time" {
+  display_name = "${google_composer_environment.this.name}-dag-file-load-time"
+  combiner = "OR"
+  conditions {
+    display_name = "DAG file load time"
+    condition_threshold {
+      threshold_value = 10
+      duration = "180s"
+      comparison = "COMPARISON_GT"
+      trigger {
+        count = 1
+      }
+      aggregations {
+        alignment_period = "60s"
+        cross_series_reducer = "REDUCE_PERCENTILE_95"
+        group_by_fields = ["resource.label.environment_name"]
+        per_series_aligner = "ALIGN_PERCENTILE_95"
+      }
+      filter = join(" ", [
+        "resource.type = \"cloud_composer_environment\"",
+        "AND resource.labels.environment_name = \"${google_composer_environment.this.name}\"",
+        "AND metric.type = \"composer.googleapis.com/environment/dag_processing/last_duration\"",
       ])
       evaluation_missing_data = "EVALUATION_MISSING_DATA_INACTIVE"
     }
