f007fbff3ec1c6f31fb834ad535b3e07585dee9a
GergelyKalmar <gergely.kalmar@logikal.io>
2026-01-21 17:58:51 +0100

    Add service accounts output

--- gcp/github-auth/outputs.tf
+++ gcp/github-auth/outputs.tf
@@ -3,6 +3,14 @@ output "workload_identity_provider" {
   value = google_iam_workload_identity_pool_provider.github.name
 }
 
+output "service_accounts" {
+  value = {
+    for service_account in keys(var.service_account_accesses) :
+    service_account => google_service_account.github_actions[service_account]
+  }
+  description = "The service accounts used with GitHub Actions"
+}
+
 output "service_account_emails" {
   value = {
     for service_account in keys(var.service_account_accesses) :
